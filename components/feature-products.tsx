b2a6e3e0c57f9ef31e4826ead77b8d89809608f4
Manu <manueljesusrejonsantana@gmail.com>
2025-02-01 17:18:37 -0600

    chore: featured products try

--- components/feature-products.tsx
+++ components/feature-products.tsx
@@ -2,19 +2,41 @@
 
 import { useGetFeaturedProducts } from "@/api/useGetFeaturedProducts"
 import { ResponseType } from "@/types/response"
-import { Carousel, CarouselContent } from "./ui/carousel"
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel"
 import SkeletonSchema from "./skeletonSchema"
+import { ProductType } from "@/types/product"
+import { Card, CardContent } from "./ui/card"
 
 const FeaturedProducts = () => {
     const { loading, result }: ResponseType = useGetFeaturedProducts()
     console.log(result);
     return (
-        <div className="max-w-6xl py-4 sm:py-16 sm:px-24 text-center">
+        <div className="max-w-6xl py-4 sm:py-16 sm:px-24">
             <h3 className="px-6 text-3xl sm:pb-8">Productos destacados</h3>
             <Carousel>
                 <CarouselContent className="-ml-2 md:-ml-4">
                     {loading && (
                         <SkeletonSchema grid={3} />
+                    )}
+                    {result !== null && (
+                        result.map((product: ProductType) => {
+                            const { attributes, id } = product
+                            const { images } = attributes
+
+                            return (
+                                <CarouselItem key={id} className="md:basis-1/2 lg:basis-1/3 group:">
+                                    <div className="p-1">
+                                        <Card className="py-4 border border-gray-200 shadow-none">
+                                            <CardContent className="relative flex items-center justify-center px-6 py-2">
+                                                <img
+                                                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images.data[0].attributes.url}`}
+                                                    alt="image featureds" />
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+                                </CarouselItem>
+                            )
+                        })
                     )}
                 </CarouselContent>
             </Carousel>
